7ff1cfd00b721af2869f9f92a6ebe6bf0c176105
brihcherry <118494838+brihcherry@users.noreply.github.com>
2023-01-25 12:30:52 -0500

    Add files via upload

--- lab1/lab1b/bikeRidePoints.cpp
+++ lab1/lab1b/bikeRidePoints.cpp
@@ -8,26 +8,37 @@ using namespace std;
 int main(){
 
     int numberRides; // number of bike rides, to be entered by user
-
     // getting input from user
     cout << "Enter the number of bike rides to campus: ";
     cin >> numberRides;
+    bool valid = 0;
 
     // using input to determine points
-    if (numberRides < 0)
-        cout << "Error: invalid number of rides entered." << endl;
-    else if (numberRides == 0)
-        cout << "You earned 0 points this semester." << endl;
-    else if (numberRides == 1)
-        cout << "You earned 3 points this semester." << endl;
-    else if (numberRides == 2)
-        cout << "You earned 10 points this semester." << endl;    
-    else if (numberRides == 3)
-        cout << "You earned 15 points this semester." << endl;
-    else if (numberRides == 4)
-        cout << "You earned 30 points this semester." << endl;
-    else if (numberRides >= 5)
-        cout << "You earned 50 points this semester." << endl;
+    while (valid == 0){
+        if (numberRides == 0){
+            cout << "You earned 0 points this semester." << endl;
+            valid = 1;}
+        else if (numberRides == 1){
+            cout << "You earned 3 points this semester." << endl;
+            valid = 1;}
+        else if (numberRides == 2){
+            cout << "You earned 10 points this semester." << endl;    
+            valid = 1;}
+        else if (numberRides == 3){
+            cout << "You earned 15 points this semester." << endl;
+            valid = 1;}
+        else if (numberRides == 4){
+            cout << "You earned 30 points this semester." << endl;
+            valid = 1;}
+        else if (numberRides >= 5){
+            cout << "You earned 50 points this semester." << endl;
+            valid = 1;}
+        else {
+            cout << "Error: enter a new number: ";
+            cin >> numberRides;}
+        }
+
+
 
 return 0; 
 }
