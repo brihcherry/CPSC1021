7663f1c93ecf22e69ee14660a99659bd8c9a80f3
brihcherry <118494838+brihcherry@users.noreply.github.com>
2023-02-06 11:28:52 -0500

    Update validateUserInput.cpp

--- lab2/validateUserInput.cpp
+++ lab2/validateUserInput.cpp
@@ -1,4 +1,4 @@
-// Names: Brianna Cherry, George Dean, Matthew Phillips, Townsend McDonald
+// Names: B------ C-----
 // Date: 1/30/23
 // Lab: Lab 2
 // Description: This file contains the function that makes sure no inputs are invalid or negative
